2bb7a645350404e69ea19218d1e0a4504be279e7
Saber94188 <shayma6026@gmail.com>
2022-07-21 18:01:24 -0700

    GUI

---
+++ GUI/TriviaMazeMain.java
@@ -0,0 +1,20 @@
+package GUI;
+
+public class TriviaMazeMain {
+	
+	public ActionHandler aHandler = new ActionHandler(this);
+	public GUI gui = new GUI(this);
+	public Event event = new Event(this);
+	
+	public static void main(String[] args) {
+		
+		new TriviaMazeMain();
+		
+	}
+	
+	public TriviaMazeMain() {
+		
+		
+	}
+
+}
