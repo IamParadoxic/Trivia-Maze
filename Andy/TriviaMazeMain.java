a10d5f8183994d3af698004489bcbb27173efac0
andylai92 <109248168+andylai92@users.noreply.github.com>
2022-08-11 19:51:52 -0700

    Add files via upload

---
+++ Andy/TriviaMazeMain.java
@@ -0,0 +1,28 @@
+package Model;
+
+import Controller.*;
+import View.*;
+import Model.*;
+
+public class TriviaMazeMain {
+	
+	public ActionHandler aHandler = new ActionHandler(this);
+	public InputHandler iHandler = new InputHandler(this);
+	public GUI gui = new GUI(this);
+	public Event event = new Event(this);
+	public Door door = new Door(0, 0, 0, 0);
+	public Question question = new Question(null, null, 0, this);
+	public Room room = new Room(0, 0, 0, 0, null);
+	
+	public static void main(String[] args) {
+		
+		new TriviaMazeMain();
+		
+	}
+	
+	public TriviaMazeMain() {
+		
+		
+	}
+
+}
