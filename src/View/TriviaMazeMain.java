63758eadfceb9e00bb6991a5cf70de005c816f0e
Harrison <Harrisonmcd1999@hotmail.com>
2022-08-14 23:47:24 -0700

    minor tweak
    just commented those line out... cause well they weren't doing anything far as I could tell

--- src/View/TriviaMazeMain.java
+++ src/View/TriviaMazeMain.java
@@ -12,9 +12,9 @@ public class TriviaMazeMain {
 	public InputHandler iHandler = new InputHandler(this);
 	public GUI gui = new GUI(this);
 	public Event event = new Event(this);
-	public Door door = new Door(0, 0, 0, 0);
-	public Room room = new Room(0, 0, 0, 0, null);
-	public Question question = new Question(null, null, 0, null, this);
+//	public Door door = new Door(0, 0, 0, 0);
+//	public Room room = new Room(0, 0, 0, 0, null);
+//	public Question question = new Question(null, null, 0, null, this);
 	
 	public static void main(String[] args) {
 		
