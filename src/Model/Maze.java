ec533d243c79fbef810b7d492dda868804caa65e
andylai92 <109248168+andylai92@users.noreply.github.com>
2022-08-17 17:13:20 -0700

    Update Maze.java

--- src/Model/Maze.java
+++ src/Model/Maze.java
@@ -7,161 +7,136 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Maze extends JPanel implements Serializable{
-	
+
 	private static final long serialVersionUID = 153585957946307993L;
-	
+
 	private final List<Rectangle> myRooms = new LinkedList<Rectangle>();
-    private final List<Rectangle> myDoors = new LinkedList<Rectangle>();
-    private final List<Rectangle> myOpenSymbols = new LinkedList<Rectangle>();
-    private final List<Rectangle> myLockedSymbols = new LinkedList<Rectangle>();
-    private Rectangle myPlayerToken;
-	
+	private final List<Rectangle> myDoors = new LinkedList<Rectangle>();
+	private final List<Rectangle> myOpenSymbols = new LinkedList<Rectangle>();
+	private final List<Rectangle> myLockedSymbols = new LinkedList<Rectangle>();
+	private Rectangle myPlayerToken;
+
 	public Maze(BorderLayout theBorderLayout) {
-		
-        super(theBorderLayout);
-        myPlayerToken = new Rectangle(0,0,2,2);
-        
+		super(theBorderLayout);
+		myPlayerToken = new Rectangle(0,0,2,2);
 	}
-	
+
 	/**
-	 * Add room into min map
+	 * Add room into mini map
 	 * 
 	 * @param theX coordinate x 
 	 * @param theY coordinate y
 	 * @param theWidth item width
 	 * @param theHeight item height
 	 */
-    public void addRoom(int theX, int theY, int theWidth, int theHeight) {
-    	
-    	myRooms.add(new Rectangle(theX,theY,theWidth,theHeight));
-    }
-    
-    /**
-	 * Add closed door into min map
+	public void addRoom(int theX, int theY, int theWidth, int theHeight) {
+		myRooms.add(new Rectangle(theX,theY,theWidth,theHeight));
+	}
+
+	/**
+	 * Add closed door into mini map
 	 * 
 	 * @param theX coordinate x 
 	 * @param theY coordinate y
 	 * @param theWidth item width
 	 * @param theHeight item height
 	 */
-    public void addDoor(int theX, int theY, int theWidth, int theHeight) {
-    	
-    	myDoors.add(new Rectangle(theX,theY,theWidth,theHeight));
-    }
-    
-    /**
-	 * Add opened door into min map
+	public void addDoor(int theX, int theY, int theWidth, int theHeight) {
+		myDoors.add(new Rectangle(theX,theY,theWidth,theHeight));
+	}
+
+	/**
+	 * Add opened door into mini map
 	 * 
 	 * @param theX coordinate x 
 	 * @param theY coordinate y
 	 * @param theWidth item width
 	 * @param theHeight item height
 	 */
-    public void addOpen(int theX, int theY, int theWidth, int theHeight) {
-    	
-    	myOpenSymbols.add(new Rectangle(theX,theY,theWidth,theHeight));
-    }
-    
-    /**
-	 * Add locked door into min map
+	public void addOpen(int theX, int theY, int theWidth, int theHeight) {
+		myOpenSymbols.add(new Rectangle(theX,theY,theWidth,theHeight));
+	}
+
+	/**
+	 * Add locked door into mini map
 	 * 
 	 * @param theX coordinate x 
 	 * @param theY coordinate y
 	 * @param theWidth item width
 	 * @param theHeight item height
 	 */
-    public void addLock(int theX, int theY, int theWidth, int theHeight) {
-    	
-    	myLockedSymbols.add(new Rectangle(theX,theY,theWidth,theHeight));
-    }
-    
-    /**
-	 * Add player token into min map
+	public void addLock(int theX, int theY, int theWidth, int theHeight) {
+		myLockedSymbols.add(new Rectangle(theX,theY,theWidth,theHeight));
+	}
+
+	/**
+	 * Add player token into mini map
 	 * 
 	 * @param theX coordinate x 
 	 * @param theY coordinate y
 	 * @param theWidth item width
 	 * @param theHeight item height
 	 */
-    public void addPlayerToken(int theX, int theY, int theWidth, int theHeight) {
-    	
-    	myPlayerToken = new Rectangle(theX, theY, theWidth, theHeight);
-    }
-    
-    /**
+	public void addPlayer(int theX, int theY, int theWidth, int theHeight) {
+		myPlayerToken = new Rectangle(theX, theY, theWidth, theHeight);
+	}
+
+	/**
 	 * Move player to a different position.
 	 * 
 	 * @param theX coordinate x 
 	 * @param theY coordinate y
 	 */
-    public void movePlayer(int theX, int theY) {
-    	
-    	myPlayerToken.setLocation(theX, theY);
-    }
-    
-    /**
-     * To see if there is player token.
-     * 
-     * @return true if there is one
-     */
-    public boolean hasPlayerToken() {
-    	
-    	boolean flag = true;
-    	
-    	if (myPlayerToken.contains(1, 1)) {
-    		flag = false;
-    	}
-    	
-    	return flag;
-    }
-    
-    /**
-     * Clear everything.
-     */
-    public void clear() {
-    	
-    	myRooms.clear();
-    	myDoors.clear();
-    	myOpenSymbols.clear();
-    	myLockedSymbols.clear();
-    	myPlayerToken = new Rectangle(0,0,2,2);	
-    	repaint();
-    	
-    }
-    
-    /**
-     * Draw things in the min map.
-     */
-    @Override
-    public void paintComponent(final Graphics theGraphics) {
-    	
-        super.paintComponent(theGraphics);
-        Graphics2D tempGraphics = (Graphics2D) theGraphics;
-        
-        for (int i = 0; i < myRooms.size(); i++) {
-        	tempGraphics.setPaint(Color.GRAY);
-        	tempGraphics.fill(myRooms.get(i));
-        }
-        
-        for (int i = 0; i < myDoors.size(); i++) {
-        	tempGraphics.setPaint(Color.LIGHT_GRAY);
-        	tempGraphics.fill(myDoors.get(i));
-        }
-        
-        for (int i = 0; i < myOpenSymbols.size(); i++) {
-        	tempGraphics.setPaint(Color.green);
-        	tempGraphics.fill(myOpenSymbols.get(i));
-        }
-        
-        for (int i = 0; i < myLockedSymbols.size(); i++) {
-        	tempGraphics.setPaint(Color.RED);
-        	tempGraphics.fill(myLockedSymbols.get(i));
-        }
+	public void movePlayer(int theX, int theY) {
+		myPlayerToken.setLocation(theX, theY);
+	}
 
-    	tempGraphics.setPaint(Color.BLUE);
-    	
-    	if (myPlayerToken != null) {
-    		tempGraphics.fill(myPlayerToken);
-    	}
-    }
+	/**
+	 * To see if there is player token.
+	 * 
+	 * @return true if there is one
+	 */
+	public boolean hasPlayer() {
+		boolean flag = true;
+
+		if (myPlayerToken.contains(1, 1)) {
+			flag = false;
+		}
+		return flag;
+	}
+
+	/**
+	 * Draw things in the mini map.
+	 */
+	@Override
+	public void paintComponent(final Graphics theGraphics) {
+		super.paintComponent(theGraphics);
+		Graphics2D graphics = (Graphics2D) theGraphics;
+
+		for (int i = 0; i < myRooms.size(); i++) {
+			graphics.setPaint(Color.GRAY);
+			graphics.fill(myRooms.get(i));
+		}
+
+		for (int i = 0; i < myDoors.size(); i++) {
+			graphics.setPaint(Color.LIGHT_GRAY);
+			graphics.fill(myDoors.get(i));
+		}
+
+		for (int i = 0; i < myOpenSymbols.size(); i++) {
+			graphics.setPaint(Color.green);
+			graphics.fill(myOpenSymbols.get(i));
+		}
+
+		for (int i = 0; i < myLockedSymbols.size(); i++) {
+			graphics.setPaint(Color.RED);
+			graphics.fill(myLockedSymbols.get(i));
+		}
+
+		graphics.setPaint(Color.BLUE);
+
+		if (myPlayerToken != null) {
+			graphics.fill(myPlayerToken);
+		}
+	}
 }
